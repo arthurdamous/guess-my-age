8723f6edad0a35832de900e2a272041fe6f639df
Arthur Damous <arthur.damous@terras.agr.br>
2022-04-05 00:30:39 -0300

    Added constants

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # guess-my-age
+
+# This project is going to guess your age, if you don't believe, try it, it's free
---
+++ app/src/main/java/br/damous/guessmyage/core/Constants.kt
@@ -0,0 +1,7 @@
+package br.damous.guessmyage.core
+
+object Constants {
+
+    const val BASE_URL = "https://api.agify.io/"
+
+}
